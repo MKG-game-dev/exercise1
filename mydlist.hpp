4bb5ef0697feaa83dfbe1834af7bfa640faf9a4f
Dongsun Kim <darkrsw@gmail.com>
2026-04-05 13:25:12 +0900

    Create mydlist.hpp

---
+++ mydlist.hpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+template<typename T>
+class MyNode
+{
+private:
+	T* e;
+	MyNode<T>* n;
+	MyNode<T>* p;
+
+public:
+	void set(T* in) { this->e = in; }
+	T* get() { return this->e; }
+	void setNext(MyNode<T>* newnext) { n = newnext; }
+	MyNode<T>* getNext() { return this->n; }
+	void setPrev(MyNode<T>* newprev) { p = newprev; }
+	MyNode<T>* getPrev() { return this->p; }
+};
+
+template<typename T>
+class MyDoublyLinkedList
+{
+private:
+	MyNode<T>* head;  // sentinel
+	MyNode<T>* tail;  // sentinel
+	int isize;
+
+public:
+	MyDoublyLinkedList()
+	{
+	}
+
+	~MyDoublyLinkedList()
+	{
+	}
+
+	bool isEmpty()
+	{
+	}
+
+	int getSize()
+	{
+	}
+
+
+	MyNode<T>* getHeadNode()
+	{
+	}
+
+	MyNode<T>* getTailNode()
+	{
+	}
+
+	void addToHead(T* in)
+	{
+	}
+
+	void removeFromHead()
+	{
+	}
+
+
+	void addToTail(T* in)
+	{
+	}
+
+	void removeFromTail()
+	{
+	}
+};
